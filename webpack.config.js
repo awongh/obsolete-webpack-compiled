8ea69e37a116ef776ba2045f20813c40f7ceb6a8
awongh <akira@awongh.com>
2019-08-23 19:53:49 +0800

    added overriding browserlist

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,21 @@ module.exports = {
       title: 'Compiled Version - obsolete webpack compiled',
       filename: 'index.html'
     }),
-     new ObsoleteWebpackPlugin({
-      template: '<div>Your browser is not supported. <button id="obsoleteClose">&times;</button></div>'
+    new ObsoleteWebpackPlugin({
+      template: '<div>Your browser is not supported. <button id="obsoleteClose">&times;</button></div>',
+      browsers: [
+        '>= 1%',
+        'last 1 major version',
+        'not dead',
+        'Chrome >= 60',
+        'Firefox >= 60',
+        'Edge >= 15.15063',
+        'Explorer 11',
+        'iOS >= 10',
+        'Safari >= 10',
+        'Android >= 6',
+        'not ExplorerMobile <= 11'
+      ]
     }),
   ]
 };
