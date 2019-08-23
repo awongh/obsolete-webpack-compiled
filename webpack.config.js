e9d7d3da00ced763b04e9139a46a4866d8ca47cc
awongh <akira@awongh.com>
2019-08-23 20:10:59 +0800

    separate file for supported browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ObsoleteWebpackPlugin = require('obsolete-webpack-plugin');
 
+const supportedBrowsers = require('./supported-browsers');
+
 module.exports = {
   entry: './index.js',
   output: {
@@ -14,19 +16,7 @@ module.exports = {
     }),
     new ObsoleteWebpackPlugin({
       template: '<div>Your browser is not supported. <button id="obsoleteClose">&times;</button></div>',
-      browsers: [
-        '>= 1%',
-        'last 1 major version',
-        'not dead',
-        'Chrome >= 60',
-        'Firefox >= 60',
-        'Edge >= 15.15063',
-        'Explorer 11',
-        'iOS >= 10',
-        'Safari >= 10',
-        'Android >= 6',
-        'not ExplorerMobile <= 11'
-      ]
-    }),
+      browsers: supportedBrowsers
+    })
   ]
 };
